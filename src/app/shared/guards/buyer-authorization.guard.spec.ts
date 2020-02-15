27530a0bee935830ea560a88c11cc5f2096527ea
Nils Heumer <nheumer@gmail.com>
2020-02-16 00:16:46 +0100

    Guards
    - Adds buyer role authoriation guard tests

--- src/app/shared/guards/buyer-authorization.guard.spec.ts
+++ src/app/shared/guards/buyer-authorization.guard.spec.ts
@@ -1,15 +1,57 @@
-import { TestBed, async, inject } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {KeycloakService} from 'keycloak-angular';
+import {LoggedInUserHelperService} from 'shared/helper/logged-in-user-helper.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
 import { BuyerAuthorizationGuard } from './buyer-authorization.guard';
 
-describe('BuyerRoleGuard', () => {
+describe('BuyerAuthorizationGuard', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BuyerAuthorizationGuard]
+      providers: [
+        BuyerAuthorizationGuard,
+        LoggedInUserHelperService,
+        HttpClient,
+        HttpHandler,
+        {
+          provide: KeycloakService,
+          useValue: new KeycloakService()
+        }
+      ]
     });
   });
 
-  it('should ...', inject([BuyerAuthorizationGuard], (guard: BuyerAuthorizationGuard) => {
-    expect(guard).toBeTruthy();
-  }));
+  it('should be created',
+      inject([BuyerAuthorizationGuard], (guard: BuyerAuthorizationGuard) => {
+        expect(guard).toBeTruthy();
+      }));
+
+  it('should authorize when logged in user has buyer role',
+      inject([BuyerAuthorizationGuard], (guard: BuyerAuthorizationGuard) => {
+        const spy = spyOn(guard.loggedInUserService, 'isBuyer').and.returnValue(true);
+
+        const result = guard.canActivate();
+
+        expect(result).toBeTruthy();
+        expect(spy).toHaveBeenCalledTimes(1);
+      }));
+
+  it('should not authorize when logged in user has not buyer role',
+      inject([BuyerAuthorizationGuard], (guard: BuyerAuthorizationGuard) => {
+        const spy = spyOn(guard.loggedInUserService, 'isBuyer').and.returnValue(false);
+
+        const result = guard.canActivate();
+
+        expect(result).toBeFalsy();
+        expect(spy).toHaveBeenCalledTimes(1);
+      }));
+
+  it('should redirect to login page when logged in user has not buyer role',
+      inject([BuyerAuthorizationGuard], (guard: BuyerAuthorizationGuard) => {
+        spyOn(guard.loggedInUserService, 'isBuyer').and.returnValue(false);
+        const spy = spyOn(guard.keycloakService, 'login').and.callFake(() => Promise.resolve());
+
+        guard.canActivate();
+
+        expect(spy).toHaveBeenCalledTimes(1);
+      }));
 });
