3ad9e79053f2076d4e9e4d093cacf783ab265125
Nils Heumer <nheumer@gmail.com>
2020-02-17 23:58:29 +0100

    Admin user management
    - Imports bootstap in index.html
    - Adds demo table
    - Adds slight layout changes

--- src/app/features/admin/component/user-management/user-management.component.ts
+++ src/app/features/admin/component/user-management/user-management.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManagementComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit(): void {
+  }
 }
