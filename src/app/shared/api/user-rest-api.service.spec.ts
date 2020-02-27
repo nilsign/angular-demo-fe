22e7d7e742b47bc5d63984b4ca2038cb839556dc
Nils Heumer <nheumer@gmail.com>
2020-02-27 14:11:13 +0100

    Test fixes
    - Adds 100% test coverage to user rest api service

--- src/app/shared/api/user-rest-api.service.spec.ts
+++ src/app/shared/api/user-rest-api.service.spec.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpHandler } from '@angular/common/http';
 import { getApiBaseUrl } from 'shared/functions/api-helper.functions';
 import { UserRestApiService } from 'shared/api/user-rest-api.service';
 import { KeycloakService } from 'keycloak-angular';
-import { of } from 'rxjs';
 
 describe('UserRestApiService', () => {
 
@@ -24,7 +23,7 @@ describe('UserRestApiService', () => {
     expect(testObj).toBeTruthy();
   });
 
-  it('should request logged in via rest api', () => {
+  it('should request all users via rest api', async () => {
     const spy1 = spyOn(testObj.adminAuthGuard, 'canActivate').and.returnValue(true);
     const spy2 = spyOn (testObj.http, 'get').and.stub();
 
@@ -33,5 +32,19 @@ describe('UserRestApiService', () => {
     expect(spy1).toHaveBeenCalledTimes(1);
     expect(spy2).toHaveBeenCalledTimes(1);
     expect(spy2).toHaveBeenCalledWith(`${getApiBaseUrl()}/user`);
+  });
+
+  it('should throw error when user is not authenticated and all users are requested', async () => {
+    const spy1 = spyOn(testObj.adminAuthGuard, 'canActivate').and.returnValue(false);
+    const spy2 = spyOn(testObj.http, 'get').and.stub();
+
+    try {
+      testObj.getAllUsers();
+    } catch (error) {
+      expect(error.message).toEqual('Authorization failed. Illegal RestAPI request.');
+    }
+
+    expect(spy1).toHaveBeenCalledTimes(1);
+    expect(spy2).not.toHaveBeenCalled();
   });
 });
