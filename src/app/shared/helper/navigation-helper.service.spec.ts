5486e7d8a4754bace11c66e00481ad354f895de6
Nils Heumer <nheumer@gmail.com>
2020-02-27 11:04:19 +0100

    Test fixes
    - Fixed navigation helper service tests

--- src/app/shared/helper/navigation-helper.service.spec.ts
+++ src/app/shared/helper/navigation-helper.service.spec.ts
@@ -1,8 +1,9 @@
-import {TestBed} from '@angular/core/testing';
-import {NavigationHelperService} from './navigation-helper.service';
-import {LoggedInUserHelperService} from 'shared/helper/logged-in-user-helper.service';
-import {HttpClient, HttpHandler} from '@angular/common/http';
-import {RouterTestingModule} from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing';
+import { NavigationHelperService } from './navigation-helper.service';
+import { LoggedInUserHelperService } from 'shared/helper/logged-in-user-helper.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { KeycloakService } from 'keycloak-angular';
 
 describe('NavigationHelperService', () => {
 
@@ -14,9 +15,10 @@ describe('NavigationHelperService', () => {
           RouterTestingModule
       ],
       providers: [
-          LoggedInUserHelperService,
           HttpClient,
           HttpHandler,
+          KeycloakService,
+          LoggedInUserHelperService
       ]
     });
     testObj = TestBed.get(NavigationHelperService);
@@ -27,32 +29,10 @@ describe('NavigationHelperService', () => {
   });
 
   it('should navigate to admin landing page depending on role', () => {
-    const spy1 = spyOn(testObj.loggedInUserHelper, 'isAdmin').and.returnValue(true);
-    const spy2 = spyOn(testObj, 'navigateToAdminsLandingPage');
-
-    testObj.navigateToRoleDependentLandingPage();
-
-    expect(spy1).toHaveBeenCalledTimes(1);
-    expect(spy2).toHaveBeenCalledTimes(1);
-  });
-
-  it('should navigate to seller landing page depending on role', () => {
-    const spy1 = spyOn(testObj.loggedInUserHelper, 'isAdmin').and.returnValue(false);
-    const spy2 = spyOn(testObj.loggedInUserHelper, 'isSeller').and.returnValue(true);
-    const spy3 = spyOn(testObj, 'navigateToSellersLandingPage');
-
-    testObj.navigateToRoleDependentLandingPage();
-
-    expect(spy1).toHaveBeenCalledTimes(1);
-    expect(spy1).toHaveBeenCalledTimes(1);
-    expect(spy3).toHaveBeenCalledTimes(1);
-  });
-
-  it('should navigate to buyers landing page depending on role', () => {
-    const spy1 = spyOn(testObj.loggedInUserHelper, 'isAdmin').and.returnValue(false);
+    const spy1 = spyOn(testObj.loggedInUserHelper, 'isBuyer').and.returnValue(false);
     const spy2 = spyOn(testObj.loggedInUserHelper, 'isSeller').and.returnValue(false);
-    const spy3 = spyOn(testObj.loggedInUserHelper, 'isBuyer').and.returnValue(true);
-    const spy4 = spyOn(testObj, 'navigateToBuyersLandingPage');
+    const spy3 = spyOn(testObj.loggedInUserHelper, 'isAdmin').and.returnValue(true);
+    const spy4 = spyOn(testObj, 'navigateToAdminsLandingPage');
 
     testObj.navigateToRoleDependentLandingPage();
 
@@ -62,10 +42,32 @@ describe('NavigationHelperService', () => {
     expect(spy4).toHaveBeenCalledTimes(1);
   });
 
+  it('should navigate to seller landing page depending on role', () => {
+    const spy1 = spyOn(testObj.loggedInUserHelper, 'isBuyer').and.returnValue(false);
+    const spy2 = spyOn(testObj.loggedInUserHelper, 'isSeller').and.returnValue(true);
+    const spy3 = spyOn(testObj, 'navigateToSellersLandingPage');
+
+    testObj.navigateToRoleDependentLandingPage();
+
+    expect(spy1).toHaveBeenCalledTimes(1);
+    expect(spy2).toHaveBeenCalledTimes(1);
+    expect(spy3).toHaveBeenCalledTimes(1);
+  });
+
+  it('should navigate to buyers landing page depending on role', () => {
+    const spy1 = spyOn(testObj.loggedInUserHelper, 'isBuyer').and.returnValue(true);
+    const spy2 = spyOn(testObj, 'navigateToBuyersLandingPage');
+
+    testObj.navigateToRoleDependentLandingPage();
+
+    expect(spy1).toHaveBeenCalledTimes(1);
+    expect(spy2).toHaveBeenCalledTimes(1);
+  });
+
   it('should log error when not landing page is available for the users roles', async () => {
-    const spy1 = spyOn(testObj.loggedInUserHelper, 'isAdmin').and.returnValue(false);
+    const spy1 = spyOn(testObj.loggedInUserHelper, 'isBuyer').and.returnValue(false);
     const spy2 = spyOn(testObj.loggedInUserHelper, 'isSeller').and.returnValue(false);
-    const spy3 = spyOn(testObj.loggedInUserHelper, 'isBuyer').and.returnValue(false);
+    const spy3 = spyOn(testObj.loggedInUserHelper, 'isAdmin').and.returnValue(false);
     const spy4 = spyOn(console, 'error');
 
     await testObj.navigateToRoleDependentLandingPage();
@@ -78,29 +80,26 @@ describe('NavigationHelperService', () => {
   });
 
   it('should navigate to admin landing page',  async () => {
-    const spy = spyOn(testObj.router, 'navigate').and.returnValue(Promise.resolve(true));
+    const spy = spyOn(testObj, 'navigateToAdminDashboard');
 
     await testObj.navigateToAdminsLandingPage();
 
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith(['admin']);
   });
 
   it('should navigate to seller landing page', async () => {
-    const spy = spyOn(testObj.router, 'navigate').and.returnValue(Promise.resolve(true));
+    const spy = spyOn(testObj, 'navigateToSellerDashboard');
 
     await testObj.navigateToSellersLandingPage();
 
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith(['seller']);
   });
 
   it('should navigate to buyer landing page', async () => {
-    const spy = spyOn(testObj.router, 'navigate').and.returnValue(Promise.resolve(true));
+    const spy = spyOn(testObj, 'navigateToShop');
 
     await testObj.navigateToBuyersLandingPage();
 
     expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenCalledWith(['buyer']);
   });
 });
