019349be4753b15397b976cb0912c60dcff130c3
Nils Heumer <nheumer@gmail.com>
2020-02-27 15:36:21 +0100

    Test fixes
    - Adds 100% test coverage to seller menu bar component.

--- src/app/shared/components/menu-bar/seller-menu-bar-items/seller-menu-bar-items.component.spec.ts
+++ src/app/shared/components/menu-bar/seller-menu-bar-items/seller-menu-bar-items.component.spec.ts
@@ -6,7 +6,7 @@ import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('SellerMenuBarItemsComponent', () => {
 
-  let component: SellerMenuBarItemsComponent;
+  let testObj: SellerMenuBarItemsComponent;
   let fixture: ComponentFixture<SellerMenuBarItemsComponent>;
 
   beforeEach(async(() => {
@@ -28,11 +28,29 @@ describe('SellerMenuBarItemsComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SellerMenuBarItemsComponent);
-    component = fixture.componentInstance;
+    testObj = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create', async () => {
+    expect(testObj).toBeTruthy();
+  });
+
+  it('should navigate to seller dashboard and set active menu index', async () => {
+    const spy = spyOn(testObj.navigationService, 'navigateToSellerDashboard').and.stub();
+
+    testObj.onDashboardMenuItemClicked();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(testObj.activeMenuItemIndex).toBe(0);
+  });
+
+  it('should navigate to products and set active menu index', async () => {
+    const spy = spyOn(testObj.navigationService, 'navigateToProducts').and.stub();
+
+    testObj.onProductsMenuItemClicked();
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(testObj.activeMenuItemIndex).toBe(1);
   });
 });
