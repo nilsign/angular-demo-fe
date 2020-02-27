a2930a6fec6ffe122d649fb653b07f5045b62a00
Nils Heumer <nheumer@gmail.com>
2020-02-27 11:41:14 +0100

    Test fixes
    - Fixes all broken show users component tests.

--- src/app/features/admin/component/users/show-users.component.spec.ts
+++ src/app/features/admin/component/users/show-users.component.spec.ts
@@ -1,14 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShowUsersComponent } from './show-users.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { KeycloakService } from 'keycloak-angular';
 
 describe('ShowUsersComponent', () => {
 
   let component: ShowUsersComponent;
   let fixture: ComponentFixture<ShowUsersComponent>;
 
+  let loadAllUsersSpy: jasmine.Spy;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShowUsersComponent ]
+      declarations: [
+          ShowUsersComponent
+      ],
+      providers: [
+          HttpClient,
+          HttpHandler,
+          KeycloakService
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +27,14 @@ describe('ShowUsersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ShowUsersComponent);
     component = fixture.componentInstance;
+
+    loadAllUsersSpy = spyOn(component, 'loadAllUsers').and.stub();
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(loadAllUsersSpy).toHaveBeenCalledTimes(1);
   });
 });
