ab76fea992d859d0a040b1037c95f7ca4084a453
Nils Heumer <nheumer@gmail.com>
2020-02-28 16:23:17 +0100

    Test fixes
    - Adds a proper obserable test for the show user component.

--- src/app/features/admin/component/users/show-users.component.spec.ts
+++ src/app/features/admin/component/users/show-users.component.spec.ts
@@ -10,6 +10,7 @@ import {
   userSuperAdmin
 } from 'testing/data/user-data.testing';
 import { of } from 'rxjs';
+import {UsersTableRowModel} from 'features/admin/component/users/users-table-row.model';
 
 describe('ShowUsersComponent', () => {
 
@@ -59,5 +60,29 @@ describe('ShowUsersComponent', () => {
     testObj.ngOnInit();
 
     expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+
+  it ('should initialize all users member on load all users', async () => {
+    const spy = spyOn(testObj.userRestApi.getAllUsers(), 'subscribe');
+
+    testObj.loadAllUsers();
+
+    testObj.userRestApi.getAllUsers().subscribe(
+        () => {},
+        () => {},
+        () => {
+          expect(spy).toHaveBeenCalledTimes(1);
+          expect(testObj.allUsers).not.toBeNull();
+
+          testObj.allUsers.subscribe((userTableRowModels: UsersTableRowModel[]) => {
+            expect(userTableRowModels.length).toBe(5);
+            expect(userTableRowModels[0].roleNames).toEqual('GLOBALADMIN');
+            expect(userTableRowModels[1].roleNames).toEqual('ADMIN');
+            expect(userTableRowModels[2].roleNames).toEqual('ADMIN, SELLER');
+            expect(userTableRowModels[3].roleNames).toEqual('SELLER');
+            expect(userTableRowModels[4].roleNames).toEqual('BUYER');
+          });
+        });
   });
 });
