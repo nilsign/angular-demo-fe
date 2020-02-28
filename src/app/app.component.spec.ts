a8c00ee5facc285c4074ab3b9d72739ed9c35a06
Nils Heumer <nheumer@gmail.com>
2020-02-28 15:08:18 +0100

    Test fixes
    - Fixes imports formattings
    - Removes unused imports

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,9 @@
 import { AppComponent } from 'app/app.component';
-import {NO_ERRORS_SCHEMA} from '@angular/core';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {RouterTestingModule} from '@angular/router/testing';
-import {HttpClient, HttpHandler} from '@angular/common/http';
-import {MenuBarComponent} from 'shared/components/menu-bar/menu-bar.component';
-import {SharedModule} from 'shared/shared.module';
-import {KeycloakService} from 'keycloak-angular';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { SharedModule } from 'shared/shared.module';
+import { KeycloakService } from 'keycloak-angular';
 
 describe('AppComponent', () => {
 
