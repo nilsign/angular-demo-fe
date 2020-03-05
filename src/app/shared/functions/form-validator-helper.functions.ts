c63728b182e77c290bfc634548fc7aaa99d92da5
Nils Heumer <nheumer@gmail.com>
2020-03-05 11:39:58 +0100

    Create user form validation
    - Removes superfluous escape chars from email validaiton regEx.

--- src/app/shared/functions/form-validator-helper.functions.ts
+++ src/app/shared/functions/form-validator-helper.functions.ts
@@ -4,7 +4,7 @@ import { isNil } from 'lodash';
 // tslint:disable-next-line:max-line-length
 // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript?page=1&tab=votes#tab-top
 // tslint:disable-next-line:max-line-length
-const regExEmailValidation = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/, 'i');
+const regExEmailValidation = new RegExp(/^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/, 'i');
 
 export function getEmailValidator(customErrorText?: string): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
