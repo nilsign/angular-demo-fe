49a7902b1eb3cda246861dd6059fdc4862a4305b
Nils Heumer <nheumer@gmail.com>
2020-03-18 17:49:43 +0100

    Create user tests
    - Fixes some broken tests.
    - Adds some missing tests.

--- src/app/shared/components/input/input-checkbox/input-checkbox.component.spec.ts
+++ src/app/shared/components/input/input-checkbox/input-checkbox.component.spec.ts
@@ -1,14 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InputCheckboxComponent } from './input-checkbox.component';
-import {FieldLabelComponent} from 'shared/components/input/field-label/field-label.component';
+import { FieldLabelComponent } from 'shared/components/input/field-label/field-label.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 describe('InputCheckboxComponent', () => {
 
-  let component: InputCheckboxComponent;
+  const formGroup = new FormGroup({ controlName: new FormControl()});
+  const controlName = 'controlName';
+  const inputFieldId = 'id';
+
+  let testObj: InputCheckboxComponent;
   let fixture: ComponentFixture<InputCheckboxComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+          ReactiveFormsModule
+      ],
       declarations: [
           InputCheckboxComponent,
           FieldLabelComponent
@@ -19,11 +27,16 @@ describe('InputCheckboxComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputCheckboxComponent);
-    component = fixture.componentInstance;
+    testObj = fixture.componentInstance;
+    testObj.formGroup = formGroup;
+    testObj.controlName = controlName;
+    testObj.inputFieldId = inputFieldId;
     fixture.detectChanges();
   });
 
   it('should create', async () => {
-    expect(component).toBeTruthy();
+    expect(testObj).toBeTruthy();
   });
+
+
 });
--- src/app/shared/components/input/input-text/input-text.component.spec.ts
+++ src/app/shared/components/input/input-text/input-text.component.spec.ts
@@ -1,15 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InputTextComponent } from './input-text.component';
-import {FieldInfoComponent} from 'shared/components/input/field-info/field-info.component';
-import {FieldLabelComponent} from 'shared/components/input/field-label/field-label.component';
+import { FieldLabelComponent } from 'shared/components/input/field-label/field-label.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FieldInfoComponent } from 'shared/components/input/field-info/field-info.component';
 
 describe('TextInputComponent', () => {
 
-  let component: InputTextComponent;
+  const formGroup = new FormGroup({ controlName: new FormControl()});
+  const controlName = 'controlName';
+  const inputFieldId = 'id';
+
+  let testObj: InputTextComponent;
   let fixture: ComponentFixture<InputTextComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule
+      ],
       declarations: [
           InputTextComponent,
           FieldLabelComponent,
@@ -21,11 +29,25 @@ describe('TextInputComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InputTextComponent);
-    component = fixture.componentInstance;
+    testObj = fixture.componentInstance;
+    testObj.formGroup = formGroup;
+    testObj.controlName = controlName;
+    testObj.inputFieldId = inputFieldId;
     fixture.detectChanges();
   });
 
   it('should create', async () => {
-    expect(component).toBeTruthy();
+    expect(testObj).toBeTruthy();
+  });
+
+  it('should mark input as untouched when value changes to empty string', async () => {
+    testObj.formGroup.controls[controlName].setValue('value');
+    const spy = spyOn(testObj.getFormControl(), 'markAsUntouched');
+
+    testObj.formGroup.controls[controlName].setValue('');
+
+    testObj.formGroup.controls[controlName].valueChanges.subscribe((value: string) => {
+      expect(spy).toHaveBeenCalledTimes(1);
+    });
   });
 });
