36a2d0d5f5d7377f71d87e34a3d142cdf363b856
Nils Heumer <nheumer@gmail.com>
2020-03-27 17:15:42 +0100

    Search user
    - Makes visible users table columns definable
    - Adds an additional edit icon column to the users table
    - Fixes missing browser tap favicon

--- src/app/features/admin/component/users-table/users-table.component.ts
+++ src/app/features/admin/component/users-table/users-table.component.ts
@@ -1,4 +1,4 @@
-import { Component , Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import { UsersTableRowModel } from 'features/admin/component/users-table/users-table-row.model';
 import { UserDto } from 'shared/api/dtos/dto-models';
 import { RoleHelperService } from 'shared/helper/role-helper.service';
@@ -10,24 +10,45 @@ import { isNil } from 'lodash';
 })
 export class UsersTableComponent implements OnChanges {
 
+  static readonly NAME_COLUMN_NAME = 'Name';
+  static readonly EMAIL_COLUMN_NAME = 'Email';
+  static readonly ROLES_COLUMN_NAME = 'Roles';
+  static readonly EDIT_ICON_COLUMN_NAME = 'Edit';
+
+  readonly nameColumnName = UsersTableComponent.EMAIL_COLUMN_NAME;
+  readonly emailColumnName = UsersTableComponent.EMAIL_COLUMN_NAME;
+  readonly rolesColumnName = UsersTableComponent.ROLES_COLUMN_NAME;
+  readonly editIconColumnName = UsersTableComponent.EDIT_ICON_COLUMN_NAME;
+
   @Input() userDtos: UserDto[];
 
-  tableModel: UsersTableRowModel[];
+  @Input() columns: Set<string> = new Set([
+    UsersTableComponent.NAME_COLUMN_NAME,
+    UsersTableComponent.EMAIL_COLUMN_NAME,
+    UsersTableComponent.ROLES_COLUMN_NAME,
+    UsersTableComponent.EDIT_ICON_COLUMN_NAME
+  ]);
+
+  tableRowModel: UsersTableRowModel[];
 
   constructor(public roleHelper: RoleHelperService) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (!isNil(changes.userDtos) && !isNil(changes.userDtos.currentValue)) {
-      this.tableModel = [];
+      this.tableRowModel = [];
       (changes.userDtos.currentValue as UserDto[]).forEach((userDto: UserDto) => {
-        this.tableModel.push({
+        this.tableRowModel.push({
             name: `${userDto.firstName} ${userDto.lastName}`,
             email: userDto.email,
             roleNames: this.getUserRoleCategories(userDto)
         });
       });
     }
+  }
+
+  showColumn(columnName: string): boolean {
+    return this.columns.has(columnName);
   }
 
   private getUserRoleCategories(user: UserDto): string {
